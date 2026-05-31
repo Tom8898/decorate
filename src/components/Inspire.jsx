684b23575ab20551173e8d608ddaca98d7ec43a4
Tom8898 <jingtao416@gmail.com>
2026-05-31 23:13:54 +1200

    v

--- src/components/Inspire.jsx
+++ src/components/Inspire.jsx
@@ -1,18 +1,25 @@
+import williamAustinImg from '../assets/1_ Ambitec x William Austin at the Aotearoa Art Fair.webp'
+import kurutaiImg from '../assets/2_ Ambitec x Kurutai at the Aotearoa Art Fair.webp'
+import showroomImg from '../assets/3_ See the new Ambitec showroom .webp'
+
 const PROJECTS = [
   {
     title: 'Ambitec x William Austin',
     sub: 'at the Aotearoa Art Fair',
     tone: 'proj-1',
+    image: williamAustinImg,
   },
   {
     title: 'Ambitec x Kurutai',
     sub: 'at the Aotearoa Art Fair',
     tone: 'proj-2',
+    image: kurutaiImg,
   },
   {
     title: 'See the new Ambitec showroom',
     sub: 'Mt Roskill, Auckland',
     tone: 'proj-3',
+    image: showroomImg,
   },
 ]
 
@@ -33,7 +40,9 @@ function Inspire() {
         <div className="inspire__grid">
           {PROJECTS.map((project) => (
             <a key={project.title} href="#" className={`project ${project.tone}`}>
-              <span className="project__media" aria-hidden="true" />
+              <span className="project__media">
+                <img src={project.image} alt={project.title} loading="lazy" />
+              </span>
               <span className="project__caption">
                 <span className="project__mark">_</span>
                 <span>
